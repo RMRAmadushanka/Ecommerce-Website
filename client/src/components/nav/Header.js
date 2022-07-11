f217a9f255007e60fa12711c89f429293a4a9f78
RMRAmadushanka <it19147260@my.sliit.lk>
2022-07-12 01:37:39 +0530

    navbar create

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,106 +1,40 @@
 import React, { useState } from "react";
+import {
+  AppstoreOutlined,
+  UserAddOutlined,
+  SettingOutlined,
+  LoginOutlined,
+} from "@ant-design/icons";
 
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Tooltip from "@mui/material/Tooltip";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
+import { Menu } from "antd";
+const { SubMenu } = Menu;
 
-export default function Header() {
-  const [anchorEl, setAnchorEl] = useState("");
-  const open = Boolean(anchorEl);
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
   const handleClick = (e) => {
-    setAnchorEl(e.currentTarget);
-  };
-  const handleClose = (e) => {
-    setAnchorEl(e);
+    setCurrent(e.key);
   };
   return (
-    <React.Fragment>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-        <Typography sx={{ minWidth: 100 }}>Contact</Typography>
-        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-          </IconButton>
-        </Tooltip>
-      </Box>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem>
-          <Avatar /> Profile
-        </MenuItem>
-        <MenuItem>
-          <Avatar /> My account
-        </MenuItem>
-        <Divider />
-        <MenuItem>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <Settings fontSize="small" />
-          </ListItemIcon>
-          Settings
-        </MenuItem>
-        <MenuItem>
-          <ListItemIcon>
-            <Logout fontSize="small" />
-          </ListItemIcon>
-          Logout
-        </MenuItem>
-      </Menu>
-    </React.Fragment>
+    <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+      <Menu.Item key="home" icon={<AppstoreOutlined />}>
+        Home
+      </Menu.Item>
+
+      <Menu.Item key="Login" icon={<LoginOutlined />}>
+        Login
+      </Menu.Item>
+
+      <Menu.Item key="Register" icon={<UserAddOutlined />}>
+        Register
+      </Menu.Item>
+
+      <SubMenu icon={<SettingOutlined />} title="UserName">
+        <Menu.Item key="setting1">option1</Menu.Item>
+        <Menu.Item key="setting2">option2</Menu.Item>
+      </SubMenu>
+    </Menu>
   );
-}
+};
+
+export default Header;
